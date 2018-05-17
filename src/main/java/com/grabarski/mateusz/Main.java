4d88b249b729a4aa5e64b77d321d0af2f7c3eb71
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-05-17 20:15:43 +0200

    Added empty line chart

--- src/main/java/com/grabarski/mateusz/Main.java
+++ src/main/java/com/grabarski/mateusz/Main.java
@@ -1,7 +1,25 @@
 package com.grabarski.mateusz;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by Mateusz Grabarski on 17.05.2018.
  */
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main_chart.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
---
+++ src/main/java/com/grabarski/mateusz/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.grabarski.mateusz.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+
+/**
+ * Created by Mateusz Grabarski on 17.05.2018.
+ */
+public class MainController {
+
+    @FXML
+    private LineChart lineChart;
+
+    public void initialize() {
+
+    }
+}
