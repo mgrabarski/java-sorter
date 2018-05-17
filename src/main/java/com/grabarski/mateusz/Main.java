605e16ba6471f23449a4c800c70f2417ad3f0f94
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-05-17 19:48:43 +0200

    Init commit

---
+++ src/main/java/com/grabarski/mateusz/Main.java
@@ -0,0 +1,7 @@
+package com.grabarski.mateusz;
+
+/**
+ * Created by Mateusz Grabarski on 17.05.2018.
+ */
+public class Main {
+}
---
+++ src/main/java/com/grabarski/mateusz/interfaces/Sorter.java
@@ -0,0 +1,11 @@
+package com.grabarski.mateusz.interfaces;
+
+/**
+ * Created by Mateusz Grabarski on 17.05.2018.
+ */
+public interface Sorter {
+
+    void sort(int[] array);
+
+    String getAlgorithmName();
+}
