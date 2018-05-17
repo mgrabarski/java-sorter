e34128a9f133708c3c3b6a0714d9bcc4a54d4b63
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-05-17 19:51:00 +0200

    Added sorters

---
+++ src/main/java/com/grabarski/mateusz/sort/HeapSorter.java
@@ -0,0 +1,17 @@
+package com.grabarski.mateusz.sort;
+
+import com.grabarski.mateusz.interfaces.Sorter;
+
+/**
+ * Created by Mateusz Grabarski on 17.05.2018.
+ */
+public class HeapSorter implements Sorter {
+
+    public void sort(int[] array) {
+
+    }
+
+    public String getAlgorithmName() {
+        return null;
+    }
+}
